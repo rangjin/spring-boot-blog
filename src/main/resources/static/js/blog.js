12876d25c024759668ce38fd632ba330d106936b
rangjin <heathaze2037@gmail.com>
2021-04-05 17:06:23 +0900

    ajax 코드 blog.js에 정리

--- src/main/resources/static/js/blog.js
+++ src/main/resources/static/js/blog.js
@@ -1,3 +1,16 @@
+let index = function () {
+    $(window).ready(function () {
+        $.ajax({
+            type: 'GET',
+            url: '/api/v1/post/?page=' + ($.getURLParam("page") === null ? 1 : $.getURLParam("page")),
+            success: function (result) {
+                boardList(result.content);
+                page(result.number, result.totalPages, result.first, result.last)
+            }
+        })
+    });
+}
+
 jQuery.extend({
     getURLParam: function(strParamName){
         var strReturn = "";
@@ -58,9 +71,67 @@ let page = function (number, totalPages, first, last) {
     $('#move-to-last').children('a').attr("href", "/?page=" + totalPages);
 }
 
+let create = function () {
+    $(window).ready(function () {
+        $.ajax({
+            type: 'GET',
+            url: '/api/v1/category',
+            success: function (results) {
+                results.forEach(result => {
+                    let html = '<option value="' + result.id + '">' + result.name + '</option>';
+
+                    $('#categoryId').append(html);
+                })
+            }
+        })
+    })
+
+    $('#post-create-button').on('click', function () {
+        let data = {
+            title : $('#title').val(),
+            content : document.getElementById('in').innerText,
+            categoryId : $('#categoryId').val(),
+            status : $('#status').val() === "" ? null : $('#status').val()
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/post/create',
+            dataType: 'json',
+            contentType: 'application/json; charset=UTF-8',
+            data: JSON.stringify(data),
+            success: function (result) {
+                if (result.validated == true) {
+                    alert("글이 등록되었습니다");
+
+                    location.href = "http://" + location.host + "/post/detail/" + result.data.id;
+                } else {
+                    $('.is-invalid').removeClass('is-invalid');
+
+                    result.data.forEach(function (error) {
+                        $('#' + error.field).addClass('is-invalid').attr('placeholder', error.defaultMessage);
+                    })
+                }
+            }
+        })
+    });
+}
+
+let detail = function () {
+    $(window).ready(function () {
+        $.ajax({
+            type: 'GET',
+            url: '/api/v1' + location.pathname,
+            success: function (result) {
+                content(result);
+            }
+        })
+    })
+}
+
 let content = function (result) {
     $('#post-title').text(result.title);
-    $('#post-updated-at').text(result.updatedAt);
+    $('#post-updated-at').text(new Date(result.updatedAt));
     $('#post-category-name').text(result.categoryName);
     $('#code').val(result.content);
 
