f4adf8b86e23e4760a88d9fce51bd5995ca8bc5b
rangjin <heathaze2037@gmail.com>
2021-04-07 10:38:43 +0900

    Post Edit Validation Error 출력 수정 & js 파일 함수 정리

--- src/main/resources/static/js/blog.js
+++ src/main/resources/static/js/blog.js
@@ -1,5 +1,5 @@
 let index = function () {
-    $(window).ready(function () {
+    $(function () {
         $.ajax({
             type: 'GET',
             url: '/api/v1/post/?page=' + ($.getURLParam("page") === null ? 1 : $.getURLParam("page")),
@@ -42,7 +42,7 @@ let boardList = function (list) {
     list.forEach(post => {
         let html = '<a href="/post/detail/' + post.id + '">';
         html += '<h2 class="post-title">' + post.title + '</h2></a>';
-        html += '<p class="post-meta">Posted at<br>Category <strong>' + post.categoryName + '</strong></p><hr>'
+        html += '<p class="post-meta">Posted at <strong>' + new Date(post.updatedAt) + '</strong><br>Category <strong>' + post.categoryName + '</strong></p><hr>'
 
         $('.post-preview').append(html);
     });
@@ -71,93 +71,137 @@ let page = function (number, totalPages, first, last) {
     $('#move-to-last').children('a').attr("href", "/?page=" + totalPages);
 }
 
-let create = function () {
-    $(window).ready(function () {
-        let md = new MdEditor('#mdeditor', {
-            uploader: 'http://local.dev/Lab/MdEditor/app/upload.php',
-            preview: true,
-            images: [
-            ]
-        });
-
-        $.ajax({
-            type: 'GET',
-            url: '/api/v1/category',
-            success: function (results) {
-                results.forEach(result => {
-                    let html = '<option value="' + result.id + '">' + result.name + '</option>';
-
-                    $('#categoryId').append(html);
-                })
-            }
-        })
-    })
-
-    $('#post-create-button').on('click', function () {
-        let data = {
-            title : $('#title').val(),
-            content : $('#mdeditor').val(),
-            categoryId : $('#categoryId').val(),
-            status : $('#status').val() === "" ? null : $('#status').val()
-        }
-
-        $.ajax({
-            type: 'POST',
-            url: '/api/v1/post/create',
-            dataType: 'json',
-            contentType: 'application/json; charset=UTF-8',
-            data: JSON.stringify(data),
-            success: function (result) {
-                if (result.validated == true) {
-                    alert("글이 등록되었습니다");
-
-                    location.href = "http://" + location.host + "/post/detail/" + result.data.id;
-                } else {
-                    $('.is-invalid').removeClass('is-invalid');
-
-                    result.data.forEach(function (error) {
-                        $('#' + error.field).addClass('is-invalid').attr('placeholder', error.defaultMessage);
-                    })
-                }
-            }
-        })
-    });
-}
-
-let detail = function () {
-    $(window).ready(function () {
-        $.ajax({
-            type: 'GET',
-            url: '/api/v1' + location.pathname,
-            success: function (result) {
-                content(result);
-            }
-        })
-    })
-}
-
-let content = function (result) {
-    $('#mdeditor').val(result.content);
-
+let mdEditor = function () {
     let md = new MdEditor('#mdeditor', {
         uploader: 'http://local.dev/Lab/MdEditor/app/upload.php',
         preview: true,
         images: [
         ]
     });
-
-    $('#out').html($('.mdeditor_render'));
-    $('#in').remove();
-
-    $('#post-title').text(result.title);
-    $('#post-updated-at').text(new Date(result.updatedAt));
-    $('#post-category-name').text(result.categoryName);
-
-
-    setButton(result.id);
 }
 
-let deleteConfirm = function (id) {
+let categorySelect = function () {
+    $.ajax({
+        type: 'GET',
+        url: '/api/v1/category',
+        success: function (results) {
+            results.forEach(result => {
+                let html = '<option value="' + result.id + '">' + result.name + '</option>';
+
+                $('#categoryId').append(html);
+            })
+        }
+    });
+}
+
+let getPostData = function () {
+    let data = {
+        title : $('#title').val(),
+        content : $('#mdeditor').val(),
+        categoryId : $('#categoryId').val(),
+        status : $('#status').val() === "" ? null : $('#status').val()
+    }
+
+    return data;
+}
+
+let printValidationError = function (errors) {
+    $('.is-invalid').removeClass('is-invalid');
+
+    errors.forEach(function (error) {
+        $('#' + error.field).addClass('is-invalid').attr('placeholder', error.defaultMessage);
+    })
+}
+
+let createPost = function () {
+    $(function () {
+        mdEditor();
+        categorySelect();
+    })
+
+    $('#post-create-button').on('click', function () {
+        $.ajax({
+            type: 'POST',
+            url: '/api/v1/post/create',
+            dataType: 'json',
+            contentType: 'application/json; charset=UTF-8',
+            data: JSON.stringify(getPostData()),
+            success: function (result) {
+                if (result.validated == true) {
+                    alert("글이 등록되었습니다");
+
+                    location.href = "http://" + location.host + "/post/detail/" + result.data.id;
+                } else {
+                    printValidationError(result.data);
+                }
+            }
+        })
+    });
+}
+
+let postDetail = function () {
+    $(function () {
+        $.ajax({
+            type: 'GET',
+            url: '/api/v1' + location.pathname,
+            success: function (result) {
+                $('#mdeditor').val(result.content);
+                mdEditor();
+
+                $('#out').html($('.mdeditor_render'));
+                $('#in').remove();
+
+                $('#post-title').text(result.title);
+                $('#post-updated-at').text(new Date(result.updatedAt));
+                $('#post-category-name').text(result.categoryName);
+
+                $('#post-edit-button').attr('href', '/post/edit/' + result.id);
+                $('#post-delete-button').attr('onclick', 'deletePost(' + result.id + ')');
+            }
+        })
+    })
+}
+
+let editPost = function () {
+    $(function () {
+        categorySelect();
+        $.ajax({
+            type: 'GET',
+            url: '/api/v1/post/detail/' + location.pathname.replace(/[^0-9]/g,''),
+            success: function (result) {
+                $(function () {
+                    $('#title').val(result.title);
+                    $('#categoryId').find($('option[value=' + result.categoryId + ']').attr('selected', 'selected'));
+                    $('#status').find($('option[value=' + result.status + ']')).attr('selected', 'selected');
+                    $('#mdeditor').val(result.content);
+
+                    mdEditor();
+                })
+            }
+        })
+    })
+
+    $('#post-edit-button').on('click', function () {
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/post/edit/' + location.pathname.replace(/[^0-9]/g,''),
+            dataType: 'json',
+            contentType: 'application/json; charset=UTF-8',
+            data: JSON.stringify(getPostData()),
+            success: function (result) {
+                if (result.validated == true) {
+                    alert("글이 수정되었습니다");
+
+                    location.href = "http://" + location.host + "/post/detail/" + result.data.id;
+                } else {
+                    printValidationError(result.data);
+                }
+            }
+        })
+    });
+}
+
+let deletePost = function (id) {
     let result = confirm('정말로 게시글을 삭제하시겠습니까?');
 
     if (result) {
@@ -165,16 +209,10 @@ let deleteConfirm = function (id) {
             type: 'DELETE',
             url: '/api/v1/post/delete/' + id,
             success: function () {
-                alert('게시글이 삭제되었습니다')
+                alert('게시글이 삭제되었습니다');
             }
         })
 
         location.href = "http://" + location.host;
     }
 }
-
-let setButton = function (id) {
-    $('#post-edit-button').attr('href', '/post/edit/' + id);
-    $('#post-delete-button').attr('onclick', 'deleteConfirm(' + id + ')');
-}
-
